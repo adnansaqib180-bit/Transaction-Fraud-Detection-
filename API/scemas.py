729eb890c8eb2525e7dab3965ff61f4d5eef3dc5
adnansaqib180-bit <adnansaqib180@gmail.com>
2026-08-13 23:06:38 +0500

    Enhance Transaction model with field annotations and validators for improved data integrity

--- API/scemas.py
+++ API/scemas.py
@@ -1,25 +1,32 @@
 import sys
 from pathlib import Path
-
 sys.path.append(str(Path(__file__).resolve().parents[1]))
-from pydantic import BaseModel , field_validator, computed_field
+from pydantic import BaseModel , field_validator, computed_field, Field
+from typing import Annotated
 from datetime import datetime, date
 from dict import merchant_rate, category_rate
 from SRC.UTILS import haversine_np
 
 class Transaction(BaseModel):
-    trans_date_trans_time : datetime 
-    merchant : str 
-    category : str
-    amt : int
-    gender : str
-    lat : float
-    long : float
-    city_pop : int
-    dob : date
-    merch_lat: float
-    merch_long : float
+    trans_date_trans_time :  Annotated[datetime, "Transaction date and time"] = Field(example="2021-01-01 12:00:00")
+    merchant : Annotated[str, "Merchant name"] = Field(example="fraud_Abbott-Rogahn")
+    category : Annotated[str, "Transaction category"] = Field(example="health_fitness")
+    amt : Annotated[int, "Transaction amount"] = Field(example=765,gt=0)
+    gender : Annotated[str, "Customer gender only M and F "] = Field(example="M",min_length=1,max_length=1)
+    lat : Annotated[float, "Customer latitude"] = Field(example=40.7128,ge=-90,le=90)
+    long : Annotated[float, "Customer longitude"] = Field(example=-74.0060,ge=-180,le=180)
+    city_pop : Annotated[int, "City population"] = Field(example=8000000,ge=1)
+    dob : Annotated[date, "Customer date of birth"] = Field(example="1990-01-01")
+    merch_lat: Annotated[float, "Merchant latitude"] = Field(example=40.7128,ge=-90,le=90)
+    merch_long : Annotated[float, "Merchant longitude"] = Field(example=-74.0060,ge=-90,le=90)
 
+
+    @field_validator('gender')
+    @classmethod
+    def gender_validator(cls,value : str ):
+        if value not in ['m','f','M','F']:
+            raise ValueError('invalid gender ')
+        return value.upper()
     @computed_field
     @property
     def unix_time(self) -> int:
@@ -50,6 +57,10 @@ class Transaction(BaseModel):
     @property
     def age(self) -> int:
        aage = (datetime.today() - self.dob).days // 365
+       if aage > 120:
+           raise ValueError('invalid age ')
+       elif aage < 1:
+           raise ValueError('inavlid age')
        return int(aage)
 
     @computed_field
