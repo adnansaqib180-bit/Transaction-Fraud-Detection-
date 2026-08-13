fc9c9d75c17438a2d3a57de5696e728d8232fa5d
adnansaqib180-bit <adnansaqib180@gmail.com>
2026-08-14 00:50:45 +0500

    Implement fraud prediction API and enhance Transaction model with additional fields and validations

---
+++ API/main.py
@@ -0,0 +1,34 @@
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+from SRC.UTILS import load_model
+import pandas as pd
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from scemas import Transaction
+
+app =  FastAPI()
+
+@app.post('/predict')
+def predict_fraud(data : Transaction):
+    model = load_model()
+
+    input_df = pd.DataFrame({
+        'amt' : data.amt,
+        'gender' : data.gender,
+        'city_pop' : data.city_pop,
+        'unix_time' : data.unix_time,
+        'hour' : data.hour,
+        'merchant_rate': data.merchant_rate,
+        'age' : data.age ,
+        'distance_km' : data.distance_km,
+        'category_rate' : data.category_rate
+    }, index=[0])
+    input_df['gender'] = input_df['gender'].map({'M': 1, 'M': 0})
+    prediction = model.predict_proba(input_df)[0][1]
+    Prediction = model.predict_proba(input_df)[0][0]
+    if prediction > 0.1 :
+        prediction = f'fraud_transaction{prediction}'
+    else :
+        prediction = f'normal_transaction{prediction} {Prediction}'
+    return JSONResponse(status_code=200,content={'prediction is ': prediction})
--- API/scemas.py
+++ API/scemas.py
@@ -3,7 +3,7 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 from pydantic import BaseModel , field_validator, computed_field, Field
 from typing import Annotated
-from datetime import datetime, date
+from datetime import datetime, date, timezone
 from dict import merchant_rate, category_rate
 from SRC.UTILS import haversine_np
 
@@ -11,14 +11,14 @@ class Transaction(BaseModel):
     trans_date_trans_time :  Annotated[datetime, "Transaction date and time"] = Field(example="2021-01-01 12:00:00")
     merchant : Annotated[str, "Merchant name"] = Field(example="fraud_Abbott-Rogahn")
     category : Annotated[str, "Transaction category"] = Field(example="health_fitness")
-    amt : Annotated[int, "Transaction amount"] = Field(example=765,gt=0)
+    amt : Annotated[float, "Transaction amount"] = Field(example=765,gt=0)
     gender : Annotated[str, "Customer gender only M and F "] = Field(example="M",min_length=1,max_length=1)
     lat : Annotated[float, "Customer latitude"] = Field(example=40.7128,ge=-90,le=90)
     long : Annotated[float, "Customer longitude"] = Field(example=-74.0060,ge=-180,le=180)
     city_pop : Annotated[int, "City population"] = Field(example=8000000,ge=1)
     dob : Annotated[date, "Customer date of birth"] = Field(example="1990-01-01")
     merch_lat: Annotated[float, "Merchant latitude"] = Field(example=40.7128,ge=-90,le=90)
-    merch_long : Annotated[float, "Merchant longitude"] = Field(example=-74.0060,ge=-90,le=90)
+    merch_long : Annotated[float, "Merchant longitude"] = Field(example=-74.0060,ge=-180,le=180)
 
 
     @field_validator('gender')
@@ -30,8 +30,9 @@ class Transaction(BaseModel):
     @computed_field
     @property
     def unix_time(self) -> int:
-        unix = self.trans_date_trans_time.timestamp()
-        return int(unix)
+        unix = self.trans_date_trans_time.astimezone(timezone.utc)
+        unix_float = unix.timestamp()
+        return int(unix_float)
     
     @computed_field
     @property
@@ -56,7 +57,7 @@ class Transaction(BaseModel):
     @computed_field
     @property
     def age(self) -> int:
-       aage = (datetime.today() - self.dob).days // 365
+       aage = (date.today() - self.dob).days // 365
        if aage > 120:
            raise ValueError('invalid age ')
        elif aage < 1:
@@ -78,13 +79,7 @@ class Transaction(BaseModel):
     @computed_field
     @property
     def merchant_rate(self) -> int:
-        if self.category not in merchant_rate:
+        if self.merchant not in merchant_rate:
             raise ValueError('invalid merchant')
         return merchant_rate[self.merchant]
 
-        
-
-    
-    #    Index(['amt', 'gender', 'city_pop', 'unix_time', 'is_fraud', 'hour',
-    #    'merchant_rate', 'age', 'distance_km', 'category_rate'],
-    #   dtype='str')
